d80b3d337ce772efefbe872865e7c474ad110137
Muhammad Rajab <m.rajab.priharsanto@gmail.com>
2019-07-11 17:05:37 +0700

    Initial Commit

---
+++ BelajarCoreMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelajarCoreMotion
+//
+//  Created by Muhammad Rajab Priharsanto on 11/07/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
